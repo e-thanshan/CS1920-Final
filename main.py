df4b1dc9ab673e89b47b2f531a4a4ce91680dce9
Jason Jiang <jasonjiangny@gmail.com>
2020-05-20 13:22:35 -0400

    render template function created, not tested

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+#! /usr/bin/python
+print('Content-type: text/html\n')
+
 print("hello world!")
+
+import os
+
+def render_template(parameter_list, **kwargs):
+    #given filename in this directory
+    print('Where are we: ' + str(os.getcwd())
+    os.chdir('templates')
+    print('How About Now: ' + str(os.getcwd())
+    try:
+        f = open(filename, 'r')
+        myFile = f.read()
+    except:
+        print ValueError('filename '+ filename + ' not found in ' + os.getcwd())
+        raise ValueError('filename '+ filename + ' not found in ' + os.getcwd())
+    for key, value in kwargs.items():
+        if type(i) == type('This is A String'):
+            myFile.replace('{{' + key + '}}', value)
+        else:
+            print('This Arg is not acceptable')
+            raise(ValueError('This Arg is not acceptable'))
+    print(myFile)
+    
