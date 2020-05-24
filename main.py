288f1af5e02dca2265e6222378e3fd3cc2262367
Jason Jiang <jasonjiangny@gmail.com>
2020-05-24 16:29:20 -0400

    {{include}} bug fixed, retesting

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ import os
 
 def render_template(filename, **kwargs):
     #given filename in this directory
-    print('Where are we: ' + str(os.getcwd())
+    #print('Where are we: ' + str(os.getcwd()))
     os.chdir('templates')
-    print('How About Now: ' + str(os.getcwd())
+    #print('How About Now: ' + str(os.getcwd()))
     try:
         f = open(filename, 'r')
         myFile = f.read()
+        f.close()
     except:
-        print ValueError('filename '+ filename + ' not found in ' + os.getcwd())
+        print('filename '+ filename + ' not found in ' + os.getcwd())
         raise ValueError('filename '+ filename + ' not found in ' + os.getcwd())
     for key, value in kwargs.items():
         if type(key) == type('This is A String'):
@@ -22,10 +23,33 @@ def render_template(filename, **kwargs):
         else:
             print('404 Known Internal Server Error')
             raise(ValueError('Key value pair for templates must be strings'))
+
+    #includes for first level embedded templates
+    x= 0
+    while True:
+        x += 1
+        print(x)
+        if '{{include' in myFile:
+            Before, throw, save = myFile.partition('{{include')
+            save, throw, After = save.partition('}}')
+            save = save.strip(" '") 
+            print(save)
+            try:
+                f = open(save, 'r')
+                include = f.read()
+                f.close()
+            except:
+                print('includes:' + 'filename '+ save + ' not found in ' + os.getcwd())
+                raise ValueError('includes:' + 'filename '+ save + ' not found in ' + os.getcwd())
+            myFile = Before + include + After
+        else:
+            break
+            
     print(myFile)
 
 # app routes
 import cgi
+
 import cgitb
 cgitb.enable(display=0, logdir='./logdir')
 
@@ -37,16 +61,17 @@ def getInput(*args): #returns the value of a bunch of key value pair, if not fou
     return inputs
 
 #figuring out the requested path // WIP
-# ~/jjiang20@moe.stuy.edu/main.py?path=home/xxx/xxx
+# ~/jjiang20@moe.stuy.edu/main.py?path=login/game
 
-path = getInput('path') 
+[path] = getInput('path') 
 
 if path == '': # just main.py -> homepage
     print(render_template('home.html'))
 
 steps = path.split('/')
 for i in steps: #going down the path
-    if i == home
+    if i == 'login':
+        pass
          
 
 
